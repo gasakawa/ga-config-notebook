c347a48f1e3cc82c81b50c4ebb6a5ce6dc8bae24
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-26 19:45:14 -0300

    feat: change zsh installation

--- configure-tools.sh
+++ configure-tools.sh
@@ -132,6 +132,18 @@ echo "**************************************************************************
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+echo "***********************************************************************************************************************\n"
+echo "installing zsh\n"
+echo "***********************************************************************************************************************\n"
+
+sudo apt install zsh -y
+zsh --version
+
+echo "***********************************************************************************************************************\n"
+echo "installing Oh My Zsh\n"
+echo "***********************************************************************************************************************\n"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "***********************************************************************************************************************\n" 
 echo 'installing autosuggestions\n' 
@@ -146,8 +158,37 @@ echo 'installing theme\n'
 echo "***********************************************************************************************************************\n" 
 
 sudo apt install fonts-firacode -y
-wget -O ~/.oh-my-zsh/themes/node.zsh-theme https://raw.githubusercontent.com/skuridin/oh-my-zsh-node-theme/master/node.zsh-theme 
-sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="node"/g' ~/.zshrc
+export ZSH_CUSTOM=~/.oh-my-zsh
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="spaceship"/g' ~/.zshrc
+
+
+echo "SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  exec_time     # Execution time
+  line_sep      # Line break
+  vi_mode       # Vi-mode indicator
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL=\"->\"
+SPACESHIP_CHAR_SUFFIX=\" \"" >> ~/.zshrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)" -y
+
+echo "zinit light zdharma/fast-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions" >> ~/.zshrc
+source ~/.zshrc
+
 
 echo "***********************************************************************************************************************\n" 
 echo 'installing meet franz\n' 
